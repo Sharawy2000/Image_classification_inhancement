e10a466d593a879bee6167c9b12f8db4349edb02
Wesam El-ShaȜrawi <mr.wesamahmed@gmail.com>
2023-12-19 18:30:24 +0200

    up

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PyQt5.QtCore import Qt,QThread,pyqtSignal
 from PyQt5.QtGui import *
 import qdarkstyle
 import sys
-import classification
+# import classification
 import predict
 import plot_metrics
 import mulit_brightness
