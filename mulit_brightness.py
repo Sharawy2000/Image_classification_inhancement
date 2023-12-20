305f0a8b33ab37b6524eb25635927a4210f2889a
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2023-12-20 12:48:44 +0200

    Refactor brightness algorithm and use parallel code for image processing

--- mulit_brightness.py
+++ mulit_brightness.py
@@ -1,29 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from multiprocessing import Pool
-from PIL import Image, ImageFilter
-import threading
-import run_threads
-
-# Increase the brightness of the images
+from PIL import Image
+import parallel_code
 
 def brightness_algorithm(image, brightness_value):
     image = image.astype(np.float64)
+    # Increase the brightness of the images
     image += brightness_value
     image = np.clip(image, 0, 255)
+
     return image.astype(np.uint8)
 
 def main(path='Images/image.jpeg',brightness_value=100,thread_numbers=5):
-    
     input_path = np.array(Image.open(path))
-
     image = input_path.astype(np.uint8)
-
-    brightened_image=run_threads.thread_num(brightness_algorithm,brightness_value,image,thread_numbers)
-
+    brightened_image=parallel_code.thread_num(brightness_algorithm,brightness_value,image,thread_numbers)
     # Image.fromarray(brightened_image).save("Results/enhanced_img.jpg")
     plt.imsave("Results/enhanced_img.jpg", brightened_image)
-
 
     return brightened_image
 
